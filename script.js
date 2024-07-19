0889a6383976aecb3594ce7da37b1b305b0bae29
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-19 15:05:35 +0300

    Add function that creates grid of squares

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const container = document.querySelector("#container");
-console.log(container);
+
 const addRowOfSquares = (numberOfSquares) => {
     let row = document.createElement("div");
     row.classList.add("flex");
-    row.style.padding = "5px";
     container.appendChild(row);
     for (let i = 0; i < numberOfSquares; i++) {
         let square = document.createElement("div");
-        // "numberOfSquares - 1" because gaps between elements is less by one then numberOfElements
-        let squareSize = (500 - 5 * (numberOfSquares - 1))/numberOfSquares;
-        console.log(square);
+        // "numberOfSquares + 1" because gaps between elements and sides is less by one then numberOfElements
+        let squareSize = ((500 - 5 * (numberOfSquares + 1))/numberOfSquares);
         square.style.cssText = `
                                 background-color: #25283D;
                                 width: ${squareSize}px;
@@ -20,4 +18,10 @@ const addRowOfSquares = (numberOfSquares) => {
     }
 }
 
-addRowOfSquares(16);
+let createGridOfSquares = (numberOfSquares) => {
+    for (let i = 0; i < numberOfSquares; i++) {
+        addRowOfSquares(numberOfSquares);
+    }
+}
+
+createGridOfSquares(16);
