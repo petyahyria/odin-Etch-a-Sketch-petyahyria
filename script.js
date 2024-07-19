822d9de02b999a952427e4136f489e6b882a6646
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-19 14:46:13 +0300

    Add function that create row of squares

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const container = document.querySelector("#container");
+console.log(container);
+const addRowOfSquares = (numberOfSquares) => {
+    let row = document.createElement("div");
+    row.classList.add("flex");
+    row.style.padding = "5px";
+    container.appendChild(row);
+    for (let i = 0; i < numberOfSquares; i++) {
+        let square = document.createElement("div");
+        // "numberOfSquares - 1" because gaps between elements is less by one then numberOfElements
+        let squareSize = (500 - 5 * (numberOfSquares - 1))/numberOfSquares;
+        console.log(square);
+        square.style.cssText = `
+                                background-color: #25283D;
+                                width: ${squareSize}px;
+                                height: ${squareSize}px;
+                                flex-shrink: 1;
+                                `
+        row.appendChild(square);
+    }
+}
 
+addRowOfSquares(16);
