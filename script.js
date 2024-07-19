c44fe6fae683fc1c73306247865535a064643d99
peter <112580184+petyahyria@users.noreply.github.com>
2024-07-19 15:54:51 +0300

    Add buuton that create new sketch

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector("#container");
+const addSketchBtn = document.createElement("button");
+addSketchBtn.textContent = "Add Scetch"
+addSketchBtn.classList.add("add-sketch-btn");
+document.body.insertBefore(addSketchBtn, container);
 
 const addRowOfSquares = (numberOfSquares) => {
     let row = document.createElement("div");
@@ -6,8 +10,9 @@ const addRowOfSquares = (numberOfSquares) => {
     container.appendChild(row);
     for (let i = 0; i < numberOfSquares; i++) {
         let square = document.createElement("div");
-        // "numberOfSquares + 1" because gaps between elements and sides is less by one then numberOfElements
-        let squareSize = ((500 - 5 * (numberOfSquares + 1))/numberOfSquares);
+        const WIDTH_OF_CONTAINER = container.offsetWidth;
+        // "numberOfSquares + 1" because gaps between elements and sides is greater by one then numberOfElements
+        let squareSize = ((WIDTH_OF_CONTAINER - 5 * (numberOfSquares + 1))/numberOfSquares);
         square.style.cssText = `
                                 background-color: #25283D;
                                 width: ${squareSize}px;
@@ -25,3 +30,24 @@ let createGridOfSquares = (numberOfSquares) => {
 }
 
 createGridOfSquares(16);
+
+let askUserNumber = () => {
+    let numberOfSquares = +prompt("Enter a number from 0 to 100:");
+        while(numberOfSquares < 0 || numberOfSquares > 100){
+            numberOfSquares = +prompt("Enter a number from 0 to 100:");
+        }
+        return numberOfSquares;
+}
+
+let deletePreviousSketch = () => {
+    while(container.firstChild){
+        container.removeChild(container.lastChild)
+    }
+}
+
+let addNewSketch = (numberOfSquares) => {
+    deletePreviousSketch();
+    createGridOfSquares(askUserNumber());
+}
+
+addSketchBtn.addEventListener("click", addNewSketch);
